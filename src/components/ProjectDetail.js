ad633ce9bf481d84ff785c72c9c430bc2322e6c5
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-11 09:52:59 -0700

    Improvements to layout of "Projects" section/cards

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shape, string } from 'prop-types'
 
+import Button from 'material-ui/Button'
 import Paper from 'material-ui/Paper'
 import Typography from 'material-ui/Typography'
 
@@ -8,26 +9,64 @@ import './ProjectDetail.css'
 
 const ProjectDetail = (props) => {
   const { project } = props
+
   return (
     <Paper className="project-detail">
       <Typography type="headline" component="h3" gutterBottom>
         {project.title}
       </Typography>
+      <Typography type="subheading" component="h4" gutterBottom>
+        {project.subtitle}
+      </Typography>
+      <Typography type="body1" component="p" gutterBottom>
+        {project.description}
+      </Typography>
+
+      <hr/>
 
       <Typography type="subheading" component="h4" gutterBottom>
-        This is the projects subtitle!
+        Related links:
       </Typography>
 
-      <Typography type="body1" component="p" gutterBottom>
-        This is a description of the project!
-      </Typography>
+      <Button
+        className="project-link-btn"
+        raised color="primary"
+        href={project.demoUrl}
+        target="_blank" rel="noopener noreferrer">
+        Demo
+      </Button>
+
+      {!!project.githubUrlClient &&
+      <Button
+        className="project-link-btn"
+        raised color="accent"
+        href={project.githubUrlClient}
+        target="_blank" rel="noopener noreferrer">
+        Github (Client)
+      </Button>
+      }
+
+      {!!project.githubUrlServer &&
+      <Button
+        className="project-link-btn"
+        raised color="accent"
+        href={project.githubUrlServer}
+        target="_blank" rel="noopener noreferrer">
+        Github (Server)
+      </Button>
+      }
     </Paper>
   )
 }
 
 ProjectDetail.propTypes = {
   project: shape({
-    title: string.isRequired
+    title: string.isRequired,
+    subtitle: string.isRequired,
+    description: string.isRequired,
+    demoUrl: string.isRequired,
+    githubUrlClient: string,
+    githubUrlServer: string
   }).isRequired
 }
 
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -65,7 +65,7 @@ function registerValidSW (swUrl) {
             }
           }
         }
-      };
+      }
     })
     .catch(error => {
       console.error('Error during service worker registration:', error)
