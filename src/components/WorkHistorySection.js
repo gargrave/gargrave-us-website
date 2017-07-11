26524ce755fd258207e2e57ccf9c60fe169fa7cd
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-11 15:32:27 -0700

    Add placeholder "work history" section

---
+++ src/components/WorkHistorySection.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import Typography from 'material-ui/Typography'
+
+class ProjectsSection extends Component {
+  render () {
+    return (
+      <section>
+        <Typography type="headline" gutterBottom>
+          Work History
+        </Typography>
+
+        <h4>TODO: Fill this out, newphew!</h4>
+      </section>
+    )
+  }
+}
+
+export default ProjectsSection
