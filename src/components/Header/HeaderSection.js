c7395dc4ebe530fc987ec5a74e3fa75c91f35e18
Gabe Hargrave <gkhargrave@gmail.com>
2017-07-24 14:45:34 -0700

    Header and Footer styling tweaks

--- src/components/Header/HeaderSection.js
+++ src/components/Header/HeaderSection.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 
 import Typography from 'material-ui/Typography'
 
+import './HeaderSection.css'
+
 class HeaderSection extends Component {
   render () {
     return (
       <section id="header">
-        <Typography type="display2">gargrave.us</Typography>
+        <Typography type="display2">gargrave</Typography>
       </section>
     )
   }
