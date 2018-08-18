1d6182d53180406f618415a1760ce8872899a6fb
Gabe Hargrave <gkhargrave@gmail.com>
2018-08-18 15:51:16 -0700

    Update dependencies

--- src/data/about.js
+++ src/data/about.js
@@ -2,16 +2,9 @@ export default [
   `
   Hi there! My name is **Gabe Hargrave**, and I am web developer. If you are looking for a treasure trove of information about me, you have come to the right place.
   Otherwise, if you're _not_ looking for me, you will probably have more fun watching [cat videos on YouTube](https://www.youtube.com/results?search_query=cat%20videos).
-  Go ahead, I won't be offended.
-  `,
+  Go ahead, I won't be offended.`,
   `
   The information below is basically everything you will find on my resume, albeit a bit expanded in some places.
   If you need to **contact** me, please refer to my resume ([download it here](https://www.dropbox.com/s/94otn62reycxas3/GabeHargrave-Resume.pdf?dl=0))
-  or find me on [LinkedIn](https://www.linkedin.com/in/gabe-hargrave-264322133/) and [GitHub](https://github.com/gargrave).
-  `,
-  `
-  As for the site itself, I built it with React, using the excellent [create-react-app](https://github.com/facebookincubator/create-react-app) as a seed, 
-  and it is hosted on an Ubuntu/Nginx Droplet at Digital Ocean. If you feel so inclined, you can peruse the full source for the site (including my current remaining TODOs)
-  [here](https://github.com/gargrave/gargrave-us-website).
-  `
+  or find me on [LinkedIn](https://www.linkedin.com/in/gabe-hargrave-264322133/) and [GitHub](https://github.com/gargrave).`
 ]
